dd30312cfb445687dfca976cbef65dc416e82f5a
Priince <tobiloba110@gmail.com>
2022-06-21 11:14:15 +0100

    introduction to javascript

--- calculator.js
+++ calculator.js
@@ -2,22 +2,36 @@ alert("This is a Basic arithmetic calculator that can perform the Addition, Subt
 
 alert('For Addition enter "1"\nFor Subtraction enter "2"\nFor Multiplication enter "3"\nFor Division enter "4"')
 
-let userOption = prompt("Enter option: ")
-let userNumber1 = parseInt(prompt("Enter first number: "))
-let userNumber2 = parseInt(prompt("Enter second number: "))
 
-if (userOption == "1") {
-    alert(userNumber1 + userNumber2)
-}
+while (true) {
+    let userOption = prompt("Enter option: ")
 
-else if (userOption == "2") {
-    alert(userNumber1 - userNumber2)
-}
+    if (userOption != "1" && userOption != "2" && userOption != "3" && userOption != "4") {
+        alert("Option has to be 1 for Addition, 2 for subtraction, 3 for Multiplication or 4 for Division")
+    }
 
-else if (userOption == "3") {
-    alert(userNumber1 * userNumber2)
-}
+    else {
+        let userNumber1 = parseInt(prompt("Enter first number: "))
+        let userNumber2 = parseInt(prompt("Enter second number: "))
 
-else if (userOption == "4") {
-    alert(userNumber1 / userNumber2)
+        if (userOption == "1") {
+            alert(userNumber1 + userNumber2)
+            break
+        }
+
+        else if (userOption == "2") {
+            alert(userNumber1 - userNumber2)
+            break
+        }
+
+        else if (userOption == "3") {
+            alert(userNumber1 * userNumber2)
+            break
+        }
+
+        else if (userOption == "4") {
+        alert(userNumber1 / userNumber2)
+        break
+        }
+    }
 }
