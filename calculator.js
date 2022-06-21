d84461c780a252a87cb6093fdcf48a4ae42a3187
Priince <tobiloba110@gmail.com>
2022-06-21 16:15:24 +0100

    introduction to javascript

--- calculator.js
+++ calculator.js
@@ -4,34 +4,40 @@ alert('For Addition enter "1"\nFor Subtraction enter "2"\nFor Multiplication ent
 
 
 while (true) {
-    let userOption = prompt("Enter option: ")
+    let userOption = prompt("Enter option: 1 (Addition), 2 (Subtraction), 3 (Multiplication), 4 (Division) ")
 
     if (userOption != "1" && userOption != "2" && userOption != "3" && userOption != "4") {
         alert("Option has to be 1 for Addition, 2 for subtraction, 3 for Multiplication or 4 for Division")
     }
 
     else {
-        let userNumber1 = parseInt(prompt("Enter first number: "))
-        let userNumber2 = parseInt(prompt("Enter second number: "))
-
-        if (userOption == "1") {
-            alert(userNumber1 + userNumber2)
-            break
+        let userNumber1 = parseFloat(prompt("Enter first number: "))
+        let userNumber2 = parseFloat(prompt("Enter second number: "))
+        
+        if (isNaN(userNumber1) || isNaN(userNumber2)) {
+            alert("Invalid input. Please enter a number.")
         }
-
-        else if (userOption == "2") {
-            alert(userNumber1 - userNumber2)
-            break
-        }
-
-        else if (userOption == "3") {
-            alert(userNumber1 * userNumber2)
-            break
-        }
-
-        else if (userOption == "4") {
-        alert(userNumber1 / userNumber2)
-        break
+        
+        else {
+            if (userOption == "1") {
+                alert(userNumber1 + userNumber2)
+                break
+            }
+    
+            else if (userOption == "2") {
+                alert(userNumber1 - userNumber2)
+                break
+            }
+    
+            else if (userOption == "3") {
+                alert(userNumber1 * userNumber2)
+                break
+            }
+    
+            else if (userOption == "4") {
+                alert(userNumber1 / userNumber2)
+                break
+            }
         }
     }
 }
